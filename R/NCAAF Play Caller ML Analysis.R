f8ceb7de8430534b36dd3a549a6a38879eb60db7
Matt <matthewdavis.wi@gmail.com>
2017-03-20 21:36:41 -0500

    Updated obsolete data for clarity, efficiency, and speed

--- R/NCAAF Play Caller ML Analysis.R
+++ R/NCAAF Play Caller ML Analysis.R
@@ -1,21 +1,21 @@
-#Set libraries ----------
+# Set libraries ----------
 library(readr); library(dplyr); library(randomForest)
 
-#Set working directory----------------
+# Set working directory --------
 setwd(".\\data")
 
-#Load Data-----------------
-# Get the files names
+# Load Data ---------
+    # Get the files names
 files <-  list.files(pattern="*.csv")
 
-# First apply read.csv, then rbind
+    # First apply read.csv, then rbind
 data <- do.call("rbind", lapply(files, function(x) read_csv(x)))
 
-# Create new data frame for analysis
+    # Rename columns to remove spaces and include periods
 colnames(data) <- make.names(colnames(data), unique=TRUE)
 
-#Clean data--------------------------------
-#Change Play Type to factor and remove all except run/pass and grab only teams coached by Brian Kelly
+# Clean data--------------------------------
+    # Change Play Types to factor, remove all plays except run/pass, and grab only teams coached by Brian Kelly
 bk.df <- data %>% 
   mutate(Play.Type = as.factor(Play.Type),
          Previous.Play.Type = as.factor(Previous.Play.Type)) %>% 
@@ -25,39 +25,42 @@ bk.df <- data %>%
            Offense.Team.Code == '140' & Year %in% c(2007, 2008, 2009) |
            Offense.Team.Code == '513' & Year %in% c(2010, 2011, 2012, 2014))
 
+    # Create test data set of most recent year of data
 test <- bk.df %>% 
   filter(Year >= 2014)
 
+    # Modify training data set to include all data up to the year of the test data
 train <- bk.df %>% 
-  filter(Year <=)
+  filter(Year <= 2013)
 
+    # Reclassify Play.Type as factor for randomForest analysis
 train$Play.Type <- factor(train$Play.Type) 
 test$Play.Type <- factor(test$Play.Type)
 
-#Random Forest--------------------
+# Random Forest--------------------
 myNtree = 501
 myMtry = 4
 myImportance = TRUE
 set.seed(415)
-#Model set up and run
+    # Model set up and run
 bk.model <- randomForest(Play.Type ~ Period.Number + Clock + Offense.Team.Code +
                              Defense.Team.Code + Offense.Points + Defense.Points +
                              Down + Distance + Spot + Drive.Number + Interceptions +
                              Fumbles + Point.Differential + DownXDistance + Previous.Play.Type + Year, 
                          data = train, ntree = myNtree, mtry = myMtry, importance = myImportance)
 
-#Plot error
+    #Plot error
 plot(bk.model, log="y")
 save(bk.model, file = "BrianKelly.RData")
 
-#Plot variable importance
+    #Plot variable importance
 varImpPlot(bk.model)
 
-#Create predictions in test tables
+    #Create predictions in test tables
 test$pred.Play.Type <- predict(bk.model, test)
 bk.model.results <- data.frame(test$Play.Type, test$pred.Play.Type)
 
-# Analyze accuracy
+    # Analyze accuracy
 bk.model.results %>% 
   mutate(correct = ifelse(test.Play.Type == test.pred.Play.Type, 1, 0)) %>% 
   summarize(accuracy = sum(correct, na.rm = TRUE) / n())
